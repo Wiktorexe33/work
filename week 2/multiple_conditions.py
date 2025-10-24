1227fdf4b2244e03c585b74ebd7b83d89a1fd185
Wiktorexe33 <62179794+Wiktorexe33@users.noreply.github.com>
2025-10-24 13:24:03 +0100

    week 2 completed, added code to demonstrate the use of a logical and operator

--- week 2/multiple_conditions.py
+++ week 2/multiple_conditions.py
@@ -9,3 +9,14 @@ elif (adventureType == "safe") or (adventureType== "long"):
     print("Taking the safe route!")
 else:
     print("Not sure which route to take.")
+
+# Ask user for what they hear and see
+print("What did I hear?")
+hear = input()
+print("What did I see?")
+see = input()
+# Determine what to output based on hear and see
+if (hear == "grr") and (see == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue.")
