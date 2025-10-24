5bae13075f4ab884fcf738565f7dbd29958f8041
Wiktorexe33 <62179794+Wiktorexe33@users.noreply.github.com>
2025-10-24 13:17:41 +0100

    added code to demonstrate use of multiple nested statements

--- week 2/nested_if.py
+++ week 2/nested_if.py
@@ -20,12 +20,26 @@ if place == "in the bedroom":
     print("Where in the bedroom should I look?")
     bedroom_place = input()
     if bedroom_place == "under the bed":
-         print("Found some shoes but no phone") # Display message
+         print("Found some shoes but no phone")
     else:
-        print("Found some mess but no phone.") # Display message
+        print("Found some mess but no phone.") 
 # Check the bathroom
 elif place == "in the bathroom":
     print("Where in the bathroom should I look?")
     bathroom_place = input()
     if bathroom_place == "in the bathtub":
-        print("Found a rubber duck but no phone") # Display message
+        print("Found a rubber duck but no phone.") 
+    else:
+        print("Found bathroom stuff but no phone.") 
+
+# Check the living room
+elif place == "in the living room":
+    print("Where in the living room should I look?")
+    lab_place = input()
+    if lab_place == "on the table":
+        print("Yes! I found my phone!")
+    else:
+         print("Found some stuff but no phone.")
+# For any other inputs
+else:
+ print("I do not know where that is but I will keep looking")
