651a4f03a017461f527b44d25091659476de1e0a
Wiktorexe33 <62179794+Wiktorexe33@users.noreply.github.com>
2025-10-24 11:51:28 +0100

    multiple nested statements, unfinished still working on it but out of time

--- week 2/nested_if.py
+++ week 2/nested_if.py
@@ -11,3 +11,21 @@ if coverType == "soft":
         print("Soft covers with coils or stitches are great for short books")
 else:
     print("Books with hard covers can be more expensive!")
+
+# Ask user where to look
+print("Where should I look?")
+place = input()
+# Check the bedroom
+if place == "in the bedroom":
+    print("Where in the bedroom should I look?")
+    bedroom_place = input()
+    if bedroom_place == "under the bed":
+         print("Found some shoes but no phone") # Display message
+    else:
+        print("Found some mess but no phone.") # Display message
+# Check the bathroom
+elif place == "in the bathroom":
+    print("Where in the bathroom should I look?")
+    bathroom_place = input()
+    if bathroom_place == "in the bathtub":
+        print("Found a rubber duck but no phone") # Display message
